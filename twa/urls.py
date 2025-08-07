2c9556a23b4c78043653da05b4276c119f84bf57
Alfred <76535763+free20064u@users.noreply.github.com>
2025-08-07 01:17:44 +0000

    master app created

--- twa/urls.py
+++ twa/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', include('general.urls')),
     path('accounts/', include('accounts.urls')),
     path('members/', include('members.urls')),
-    #path('extraction/', include('extraction.urls')),
+    path('master/', include('master.urls')),
     #re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     #re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]#+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
