ff43fd9c784d5300d8e57af59d4cfe3a68f2b306
Alfred <free20064u@gmail.com>
2025-08-15 00:15:18 +0000

    contact constitution and about page created

--- general/urls.py
+++ general/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('about/', views.about_page, name='about'),
+    path('contact/', views.contact_page, name='contact'),
+    path('constitution/', views.constitution_page, name='constitution'),
 ]
--- general/views.py
+++ general/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
 
-    return render(request, 'general/index.html', {'footer':False,})
+    return render(request, 'general/index.html', {'navbar':True})
+
+def about_page(request):
+    return render(request, 'general/about.html', {'navbar': True})
+
+def contact_page(request):
+    return render(request, 'general/contact.html', {'navbar': True})
+
+def constitution_page(request):
+    return render(request, 'general/constitution.html', {'navbar': True})
